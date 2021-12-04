31a91437f797c1401e444e187316337c99f40b3b
Dan <35927536+dan-mba@users.noreply.github.com>
2021-12-04 09:20:41 -0500

    upgrade to CRA5 & dependency cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  framework: '@storybook/react',
+  features: {
+    storyStoreV7: true
+  },
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  core: {
+    builder: 'webpack5'
+  }
+};
