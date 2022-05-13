063c4514102c1654a654dac1d516eb2039b4144f
Dan <35927536+dan-mba@users.noreply.github.com>
2022-05-13 07:46:22 -0400

    fix iframe assets dir

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
+// use `mergeConfig` to recursively merge Vite options
+const { mergeConfig } = require('vite');
+
 module.exports = {
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      base: './'
+    });
+  },
   framework: '@storybook/react',
   features: {
     storyStoreV7: true
