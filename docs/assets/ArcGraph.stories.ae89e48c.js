c92c0d7c0fc870630ae0422898f47900c8c2fd02
GitHub Action <action@github.com>
2022-08-06 10:34:12 +0000

    Build Action

---
+++ docs/assets/ArcGraph.stories.ae89e48c.js
@@ -0,0 +1,43 @@
+import{p as n}from"./index.d9fbabd1.js";import{i as m}from"./percentage.74b6045d.js";import{a as u,j as a}from"./jsx-runtime.c37e017f.js";import"./iframe.9acb4ee3.js";const f="_progress_lodek_7",g="_animate_lodek_1",h="_move_lodek_14",b="_percentage_lodek_19",t={progress:f,animate:g,move:h,percentage:b},c=({size:o,percentage:e,color:s,backgroundColor:l,textColor:i})=>{if(typeof e!="number"||e<0||e>100)return null;const r=200*Math.PI,d=e/100*r;return u("svg",{xmlns:"http://www.w3.org/2000/svg",width:o,height:o*.5,viewBox:"0 0 240 120",children:[a("circle",{cx:"120",cy:"123",r:"100",fill:"none",strokeWidth:"25",strokeDasharray:`${r*.5} ${r}`,strokeLinecap:"butt",stroke:l,className:t.move}),a("circle",{cx:"120",cy:"123",r:"100",fill:"none",strokeWidth:"25",stroke:s,strokeDasharray:`${d*.5} ${r}`,strokeLinecap:"butt",className:`${t.progress} ${t.move}`}),a("text",{x:"125",y:"115",fill:i,className:t.percentage,children:`${e}%`})]})};c.defaultProps={size:500,color:"blue",backgroundColor:"#efefef",textColor:"black"};c.propTypes={size:n.exports.number,percentage:m,color:n.exports.string,backgroundColor:n.exports.string,textColor:n.exports.string};const p=c;c.__docgenInfo={description:"",methods:[],displayName:"CircleGraph",props:{size:{defaultValue:{value:"500",computed:!1},type:{name:"number"},required:!1,description:""},color:{defaultValue:{value:"'blue'",computed:!1},type:{name:"string"},required:!1,description:""},backgroundColor:{defaultValue:{value:"'#efefef'",computed:!1},type:{name:"string"},required:!1,description:""},textColor:{defaultValue:{value:"'black'",computed:!1},type:{name:"string"},required:!1,description:""},percentage:{type:{name:"custom",raw:"function isPercentage(props, propName, componentName) {\n  if(typeof props[propName] === 'number') {\n    if(props[propName] < 0 || props[propName] > 100) {\n      return new Error(`Invalid prop ${propName} passed to ${componentName}. Expected a number between 0 - 100`);\n    }\n  } else {\n    return new Error(`Invalid prop ${propName} passed to ${componentName}. Expected a number`);\n  }\n}"},required:!1,description:""}}};const G={parameters:{storySource:{source:`import AG from './ArcGraph';
+
+export default {
+  title: 'Components/ArcGraph',
+  component: AG,
+  argTypes: {
+    percentage: {
+      description: 'percentage to be displayed',
+      control: 'range',
+      min: 0,
+      max: 100
+    },
+    size: {
+      description: 'width & height of graph',
+      default: 500
+    },
+    color: {
+      description: 'fill color for graph',
+      control: 'color'
+    },
+    backgroundColor: {
+      description: 'background color for unfilled area of graph',
+      control: 'color'
+    },
+    textColor: {
+      description: 'color of text percentage',
+      control: 'color'
+    }
+  },
+};
+
+const Template = (args) => <AG {...args} />;
+
+export const ArcGraph = Template.bind({});
+ArcGraph.args = {
+  percentage: 66,
+  size: 500,
+  color: 'blue',
+  backgroundColor: '#efefef',
+  textColor: 'black'
+};
+`,locationsMap:{"arc-graph":{startLoc:{col:17,line:32},endLoc:{col:43,line:32},startBody:{col:17,line:32},endBody:{col:43,line:32}}}}},title:"Components/ArcGraph",component:p,argTypes:{percentage:{description:"percentage to be displayed",control:"range",min:0,max:100},size:{description:"width & height of graph",default:500},color:{description:"fill color for graph",control:"color"},backgroundColor:{description:"background color for unfilled area of graph",control:"color"},textColor:{description:"color of text percentage",control:"color"}}},x=o=>a(p,{...o}),k=x.bind({});k.args={percentage:66,size:500,color:"blue",backgroundColor:"#efefef",textColor:"black"};const N=["ArcGraph"];export{k as ArcGraph,N as __namedExportsOrder,G as default};
+//# sourceMappingURL=ArcGraph.stories.ae89e48c.js.map
---
+++ docs/assets/es.regexp.flags.d351fc3e.js
@@ -0,0 +1,2 @@
+import{E as v,o as p,aO as u,aP as d,C as y}from"./iframe.9acb4ee3.js";var E=v,O=p,P=u,R=d,b=y,o=E.RegExp,g=o.prototype,m=O&&b(function(){var r=!0;try{o(".","d")}catch{r=!1}var a={},t="",s=r?"dgimsy":"gimsy",c=function(l,f){Object.defineProperty(a,l,{get:function(){return t+=f,!0}})},e={dotAll:"s",global:"g",ignoreCase:"i",multiline:"m",sticky:"y"};r&&(e.hasIndices="d");for(var i in e)c(i,e[i]);var n=Object.getOwnPropertyDescriptor(g,"flags").get.call(a);return n!==s||t!==s});m&&P(g,"flags",{configurable:!0,get:R});
+//# sourceMappingURL=es.regexp.flags.d351fc3e.js.map
