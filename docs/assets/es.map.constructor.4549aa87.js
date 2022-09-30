4b38fe1de2b78f5c6b4c9faf04794022d90fae15
GitHub Action <action@github.com>
2022-09-30 09:09:43 +0000

    Build Action

--- docs/assets/es.map.constructor.4549aa87.js
+++ docs/assets/es.map.constructor.4549aa87.js
@@ -1,2 +1,2 @@
-import{n as o,o as t}from"./iframe.fdda414e.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
+var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{n as e,o as c}from"./iframe.fdda414e.js";var i=e,l=c;i("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},l);
 //# sourceMappingURL=es.map.constructor.4549aa87.js.map
--- docs/assets/string.d0e94f55.js
+++ docs/assets/string.d0e94f55.js
@@ -1,2 +1,2 @@
-import{p as e}from"./iframe.fdda414e.js";var i=e;i({target:"Number",stat:!0},{isNaN:function(r){return r!=r}});var s=function(r){if(!r)return"";if(typeof r=="string")return r;throw new Error("Description: expected string, got: ".concat(JSON.stringify(r)))};export{s};
+var i=Object.defineProperty;var e=(t,r)=>i(t,"name",{value:r,configurable:!0});import{p as n}from"./iframe.fdda414e.js";var s=n;s({target:"Number",stat:!0},{isNaN:e(function(r){return r!=r},"isNaN")});var f=e(function(r){if(!r)return"";if(typeof r=="string")return r;throw new Error("Description: expected string, got: ".concat(JSON.stringify(r)))},"str");export{f as s};
 //# sourceMappingURL=string.d0e94f55.js.map
