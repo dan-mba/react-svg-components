c7778f418396bea10618d226101b614c8c6b1bd5
GitHub Action <action@github.com>
2022-12-10 11:59:44 +0000

    Build Action

--- docs/assets/es.number.is-nan.4efbacb6.js
+++
@@ -1,2 +0,0 @@
-var N=Object.defineProperty;var a=(r,t)=>N(r,"name",{value:t,configurable:!0});import{l as e}from"./iframe.bf8810e9.js";var i=e;i({target:"Number",stat:!0},{isNaN:a(function(t){return t!=t},"isNaN")});
-//# sourceMappingURL=es.number.is-nan.4efbacb6.js.map
---
+++ docs/assets/string.bc7f62d7.js
@@ -0,0 +1,2 @@
+var i=Object.defineProperty;var e=(t,r)=>i(t,"name",{value:r,configurable:!0});import{p as n}from"./iframe.2da15ea4.js";var s=n;s({target:"Number",stat:!0},{isNaN:e(function(r){return r!=r},"isNaN")});var f=e(function(r){if(!r)return"";if(typeof r=="string")return r;throw new Error("Description: expected string, got: ".concat(JSON.stringify(r)))},"str");export{f as s};
+//# sourceMappingURL=string.bc7f62d7.js.map
