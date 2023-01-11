cafced7a0afa5df44f297b62e9076f48b0991fce
Dan <35927536+dan-mba@users.noreply.github.com>
2023-01-11 13:09:11 -0500

    more migration updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 // use `mergeConfig` to recursively merge Vite options
-const { mergeConfig } = require('vite');
-
+const {
+  mergeConfig
+} = require('vite');
 module.exports = {
-  async viteFinal(config, { configType }) {
+  async viteFinal(config, {
+    configType
+  }) {
     // return the customized config
     return mergeConfig(config, {
       // customize the Vite config here
@@ -13,13 +16,7 @@ module.exports = {
     name: '@storybook/react-vite',
     options: {}
   },
-  features: {
-    storyStoreV7: true
-  },
   stories: ['../src/components/**/*.stories.jsx'],
   addons: ['@storybook/addon-essentials'],
   core: {},
-  docsPage: {
-    docs: 'automatic'
-  }
 };
