271d61344af9d5510b32a78288050593b2b49595
Dan <35927536+dan-mba@users.noreply.github.com>
2023-01-24 12:41:31 -0500

    Switch to GitHub Pages Action

--- docs/assets/percentage-0b4677c6.js
+++
@@ -1,2 +0,0 @@
-var i=Object.defineProperty;var t=(n,e)=>i(n,"name",{value:e,configurable:!0});function u(n,e,r){if(typeof n[e]=="number"){if(n[e]<0||n[e]>100)return new Error(`Invalid prop ${e} passed to ${r}. Expected a number between 0 - 100`)}else return new Error(`Invalid prop ${e} passed to ${r}. Expected a number`)}t(u,"isPercentage");export{u as i};
-//# sourceMappingURL=percentage-0b4677c6.js.map
