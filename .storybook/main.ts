f5fb399cb17f303073c0e4026de88c4fd4f18c4c
Dan <35927536+dan-mba@users.noreply.github.com>
2023-12-18 08:07:55 -0500

    dependency updates & cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [ "@storybook/addon-essentials" ],
   framework: {
     name: "@storybook/react-vite",
