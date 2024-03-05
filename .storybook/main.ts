3c7a5138a79c910ac801f63bcb8acae0a18a054a
Dan <35927536+dan-mba@users.noreply.github.com>
2024-03-05 08:08:37 -0500

    fix Vite CJS Warning

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,6 +13,7 @@ const config: StorybookConfig = {
   async viteFinal(config, {
     configType
   }) {
+    const { mergeConfig } = await import('vite');
     // return the customized config
     return mergeConfig(config, {
       // customize the Vite config here
