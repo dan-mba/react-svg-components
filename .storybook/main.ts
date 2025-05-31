1ce2b1c897ab0bce37c065b513e6df1747ac69b0
Dan <35927536+dan-mba@users.noreply.github.com>
2025-05-31 08:10:57 -0400

    storybook v9 cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,13 +2,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [ "@storybook/addon-essentials" ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: false,
   },
   async viteFinal(config) {
     const { mergeConfig } = await import('vite');
